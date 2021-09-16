ce79ec6d1abedc972b251459083b5f19120af8ba
fsdexter <fsdexter@gmail.com>
2021-09-16 01:06:53 +0000

    Endpoints  done

--- src/main.py
+++ src/main.py
@@ -42,140 +42,155 @@ def handle_hello():
 
 
 
-    # [GET] /people Get a list of all the people in the database
-    @app.route('/people', methods=["GET"])
-    def get_all_peoples():
-        
-    peoples_list = []
-    response_list = Peoples.query.all()
+# [GET] /people Get a list of all the people in the database
+@app.route('/people', methods=["GET"])
+def get_all_peoples():
+  peoples_list = []
+  response_list = Peoples.query.all()
     
-    for people in response_list:
-        peoples_list.append(people.serialize())
+  for people in response_list:
+    peoples_list.append(people.serialize())
     
-    if peoples_list is not None:
+    if len(peoples_list) !== 0:
         return jsonify(peoples_list), 200
     else:
         return "People not found", 404
-    
-    
-
-  
-    # [GET] /people/<int:people_id> Get a one single people information
-    @app.route('/people/<int:people_id>', methods=["GET"])
-
-    def get_people_By_id(people_id):
-    result = next((people for people in peoples if people["People_id"] == people_id), None)
+ 
+# [GET] /people/<int:people_id> Get a one single people information
+@app.route('/people/<int:people_id>', methods=["GET"])
+def get_people_By_id(people_id):
+  result = next((people for people in peoples_list if people["People_id"] == people_id), None)
     if result is not None:
         return jsonify(result), 200
     else:
         return "People not found", 404
 
-    # [GET] /planets Get a list of all the planets in the database
-    @app.route('/planet', methods=["GET"])
-    def get_all_planets():
-        
-    planets_list = []
-    response_list = planets.query.all()
+# [GET] /planets Get a list of all the planets in the database
+@app.route('/planet', methods=["GET"])
+def get_all_planets():
+  planets_list = []
+  response_list = Planets.query.all()
     
-    for planet in response_list:
-        planets_list.append(Planet.serialize())
+  for planet in response_list:
+    planets_list.append(planet.serialize())
     
-    if planets_list is not None:
+    if len(planets_list) !== 0:
         return jsonify(planets_list), 200
     else:
         return "Planet not found", 404
-
+ 
     
    
-    # [GET] /planets/<int:planet_id> Get one single planet information
- 
+# [GET] /planets/<int:planet_id> Get one single planet information
+@app.route('/planet/<int:planet_id>', methods=["GET"])
+def get_planet_By_id(planet_id):
+  result = next((planet for planet in planets_list if planet["Planet_id"] == planet_id), None)
+    if result is not None:
+        return jsonify(result), 200
+    else:
+        return "Planet not found", 404
 
-@app.route('/users/<string:username>', methods=['GET'])
-def getUsersByUsername(username):
-  result = next((user for user in users if user["Username"] == username), None)
-  if result is not None:
-    return jsonify(result), 200
-  else:
-    return "User not found", 404
-    # [GET] /users Get a list of all the blog post users
-    @app.route('/users', methods=["GET"])
-def getAllUsers():
-  return jsonify(users), 200
+# [GET] /users Get a list of all the blog post users
+@app.route('/user', methods=["GET"])
+def get_all_users():
+  users_list = []
+  response_list = Users.query.all()
+    
+  for user in response_list:
+    users_list.append(user.serialize())
+    
+    if len(users_list) !== 0:
+        return jsonify(users_list), 200
+    else:
+        return "User not found", 404
 
-@app.route('/users/<string:username>', methods=['GET'])
-def getUsersByUsername(username):
-  result = next((user for user in users if user["Username"] == username), None)
-  if result is not None:
-    return jsonify(result), 200
-  else:
-    return "User not found", 404
-    # [GET] /users/favorites Get all the favorites that belong to the current user.
-    @app.route('/users', methods=["GET"])
-def getAllUsers():
-  return jsonify(users), 200
+# [GET] /users/favorites Get all the favorites that belong to the current user.
+@app.route('/user/favorites', methods=["GET"])
+def get_user_favorites():
+  user_favorites_list = []
+  response_list = User_favorites.query.all()
+    
+  for user_favorite in response_list:
+    user_favorites_list.append(user_favorite.serialize())
+    
+    if len(user_favorites_list) !== 0:
+        return jsonify(user_favorites_list), 200
+    else:
+        return "User favorite not found", 404
 
-@app.route('/users/<string:username>', methods=['GET'])
-def getUsersByUsername(username):
-  result = next((user for user in users if user["Username"] == username), None)
-  if result is not None:
-    return jsonify(result), 200
-  else:
-    return "User not found", 404
-    # [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
-    @app.route('/users', methods=["POST"])
-def addUser():
-  body = json.loads(request.data)
+# [POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
+@app.route('/favorite/planet/<int:planet_id> ', methods=["POST"])
+def favorite_planets_list():
+    body_request = request.get_json()
+    favourite_list = []
+    
+    user_id_request = body_request.get("user_id", None)
+    planet_id_request = body_request.get("planet_id", None)
+        
+    favourite_list = FavoritePlanets(
+      user_id = user_id_request,
+      planet_id = planet_id_request
+    )
 
-  userName = body["Username"]
-  age = body["Age"]
+    db.session.add(favourite_list)
+    db.session.commit()
+    return jsonify(favourite_list.serialize()), 201
 
-  newUser = {
-    "Username": userName,
-    "Age": age
-  }
+# [POST] /favorite/people/<int:people_id> Add a new favorite people to the current user with the people id = people_id.
+@app.route('/favorite/people/<int:people_id>', methods=["POST"])
+def favorite_peoples_list():
+    body_request = request.get_json()
+    favourite_list = []
+    
+    user_id_request = body_request.get("user_id", None)
+    people_id_request = body_request.get("people_id", None)
+        
+    favourite_list = FavoritePeoples(
+      user_id = user_id_request,
+      people_id = people_id_request
+    )
 
-  users.append(newUser)
-  return jsonify(newUser), 200
-    # [POST] /favorite/people/<int:planet_id> Add a new favorite people to the current user with the people id = people_id.
-    @app.route('/users', methods=["POST"])
-def addUser():
-  body = json.loads(request.data)
+    db.session.add(favourite_list)
+    db.session.commit()
+    return jsonify(favourite_list.serialize()), 201
 
-  userName = body["Username"]
-  age = body["Age"]
+# [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
+@app.route('/favorite/planet/<int:planet_id>', methods=["DELETE"])
+def delete_favorite_planet():
+  body_request = request.get_json()
+  favourite_list = []
+    
+  user_id_request = body_request.get("user_id", None)
+  planet_id_request = body_request.get("planet_id", None)
+        
+  delete_favorite_planet = FavoritePlanets.query.filter_by(user_id = user_id_request, planet_id = planet_id_request).first_or_404()
 
-  newUser = {
-    "Username": userName,
-    "Age": age
-  }
+  try:
+      db.session.delete(delete_favorite_planet)
+      db.session.commit()
+      return jsonify({"msg":"Planet Successfully Deleted "}), 200
+            
+  except:
+      return "ERROR PLANET NOT DELETED", 400
 
-  users.append(newUser)
-  return jsonify(newUser), 200
-    # [DELETE] /favorite/planet/<int:planet_id> Delete favorite planet with the id = planet_id.
-    @app.route('/users/<string:username>', methods=["DELETE"])
-def deleteUser(username):
-  userFound = None
-  for index, user in enumerate(users):
-    if user["Username"] == username:
-      userFound = user
-      users.pop(index)
-  if userFound is not None:
-    return "User deleted", 200
-  else:
-    return "User not found", 404
+# [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
+@app.route('/favorite/people/<int:people_id>', methods=["DELETE"])
+def delete_favorite_people():
+  body_request = request.get_json()
+  favourite_list = []
+    
+  user_id_request = body_request.get("user_id", None)
+  people_id_request = body_request.get("people_id", None)
+        
+  delete_favorite_people = FavoritePeoples.query.filter_by(user_id = user_id_request, people_id = people_id_request).first_or_404()
 
-    # [DELETE] /favorite/people/<int:people_id> Delete favorite people with the id = people_id.
-    @app.route('/users/<string:username>', methods=["DELETE"])
-def deleteUser(username):
-  userFound = None
-  for index, user in enumerate(users):
-    if user["Username"] == username:
-      userFound = user
-      users.pop(index)
-  if userFound is not None:
-    return "User deleted", 200
-  else:
-    return "User not found", 404
+  try:
+      db.session.delete(delete_favorite_people)
+      db.session.commit()
+      return jsonify({"msg":"People Successfully Deleted "}), 200
+            
+  except:
+      return "ERROR PEOPLE NOT DELETED", 400
 
 
 # this only runs if `$ python src/main.py` is executed
